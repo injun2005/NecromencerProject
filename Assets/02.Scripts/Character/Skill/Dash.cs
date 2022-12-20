ea44c1aa6afbec4e6f4af3849a50c439c4eb59e3
chogunny1 <gunnychocho@gmail.com>
2022-12-20 09:54:59 +0900

    이펙트 업로드 및 대쉬 파이이볼 이펙트 추가 2022 12 20
    이펙트 업로드 및 대쉬 파이이볼 이펙트 추가 2022 12 20

--- Assets/02.Scripts/Character/Skill/Dash.cs
+++ Assets/02.Scripts/Character/Skill/Dash.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Dash : Skill
 {
+    public GameObject SkillEffect;
     public override void UseSkill(Character skillTarget)
     {
         Debug.Log("Dash");
+        StartCoroutine(EffectSkill());
         int addDamage = character.Speed - skillTarget.Speed > 0 ? character.Speed - skillTarget.Speed : 0;
-
         character.Attack(character.AD + addDamage);
+    }
+
+    public IEnumerator EffectSkill()
+    {
+        GameObject GO = (GameObject)Instantiate(SkillEffect, character.transform);
+        yield return new WaitForSeconds(2f);
+        Destroy(GO);
     }
 }
--- Assets/02.Scripts/Character/Skill/Fireball.cs
+++ Assets/02.Scripts/Character/Skill/Fireball.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Fireball : Skill
 {
+    public GameObject SkillEffect;
     public override void UseSkill(Character skillTarget)
     {
         Debug.Log("Fireball");
+        StartCoroutine(EffectSkill());
         character.Attack(character.AD + 2);
+    }
+
+    public IEnumerator EffectSkill()
+    {
+        GameObject GO = (GameObject)Instantiate(SkillEffect,character.transform);
+        yield return new WaitForSeconds(2f);
+        Destroy(GO);
     }
 }
